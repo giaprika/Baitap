56ef8e996cdcb58666170e7c6df14be9dc24d67c
your name <143941671+giaprika@users.noreply.github.com>
2024-03-10 16:38:43 +0700

    Update main.cpp

--- Tuan4/Bai6/main.cpp
+++ Tuan4/Bai6/main.cpp
@@ -1,9 +1,229 @@
-#include <iostream>
 
+#include <bits/stdc++.h>
 using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+void print(char s, int i){
+    if(s=='A'){
+        if(i==1) cout<<" #### ";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"#    #";
+        if(i==4) cout<<"######";
+        if(i==5) cout<<"#    #";
+        if(i==6) cout<<"#    #";
+    }
+    if(s=='B'){
+        if(i==1) cout<<"##### ";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"######";
+        if(i==4) cout<<"#    #";
+        if(i==5) cout<<"#    #";
+        if(i==6) cout<<"##### ";
+    }
+    if(s=='C'){
+        if(i==1) cout<<"######";
+        if(i==2) cout<<"#     ";
+        if(i==3) cout<<"#     ";
+        if(i==4) cout<<"#     ";
+        if(i==5) cout<<"#     ";
+        if(i==6) cout<<"######";
+    }
+    if(s=='D'){
+        if(i==1) cout<<"##### ";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"#    #";
+        if(i==4) cout<<"#    #";
+        if(i==5) cout<<"#    #";
+        if(i==6) cout<<"##### ";
+    }
+    if(s=='E'){
+        if(i==1) cout<<"######";
+        if(i==2) cout<<"#     ";
+        if(i==3) cout<<"######";
+        if(i==4) cout<<"#     ";
+        if(i==5) cout<<"#     ";
+        if(i==6) cout<<"######";
+    }
+    if(s=='F'){
+        if(i==1) cout<<"######";
+        if(i==2) cout<<"#     ";
+        if(i==3) cout<<"######";
+        if(i==4) cout<<"#     ";
+        if(i==5) cout<<"#     ";
+        if(i==6) cout<<"#     ";
+    }
+    if(s=='G'){
+        if(i==1) cout<<" #### ";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"#     ";
+        if(i==4) cout<<"#  ###";
+        if(i==5) cout<<"#    #";
+        if(i==6) cout<<" #### ";
+    }
+    if(s=='H'){
+        if(i==1) cout<<"#    #";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"######";
+        if(i==4) cout<<"#    #";
+        if(i==5) cout<<"#    #";
+        if(i==6) cout<<"#    #";
+    }
+    if(s=='I'){
+        if(i==1) cout<<"  #   ";
+        if(i==2) cout<<"  #   ";
+        if(i==3) cout<<"  #   ";
+        if(i==4) cout<<"  #   ";
+        if(i==5) cout<<"  #   ";
+        if(i==6) cout<<"  #   ";
+    }
+    if(s=='J'){
+        if(i==1) cout<<"######";
+        if(i==2) cout<<"     #";
+        if(i==3) cout<<"     #";
+        if(i==4) cout<<"#    #";
+        if(i==5) cout<<"#    #";
+        if(i==6) cout<<" #### ";
+    }
+    if(s=='K'){
+        if(i==1) cout<<"#    #";
+        if(i==2) cout<<"#   # ";
+        if(i==3) cout<<"####  ";
+        if(i==4) cout<<"#  #  ";
+        if(i==5) cout<<"#   # ";
+        if(i==6) cout<<"#    #";
+    }
+    if(s=='L'){
+        if(i==1) cout<<"#     ";
+        if(i==2) cout<<"#     ";
+        if(i==3) cout<<"#     ";
+        if(i==4) cout<<"#     ";
+        if(i==5) cout<<"#     ";
+        if(i==6) cout<<"######";
+    }
+    if(s=='M'){
+        if(i==1) cout<<"#    #";
+        if(i==2) cout<<"##  ##";
+        if(i==3) cout<<"# ## #";
+        if(i==4) cout<<"#    #";
+        if(i==5) cout<<"#    #";
+        if(i==6) cout<<"#    #";
+    }
+    if(s=='N'){
+        if(i==1) cout<<"#    #";
+        if(i==2) cout<<"##   #";
+        if(i==3) cout<<"# #  #";
+        if(i==4) cout<<"#  # #";
+        if(i==5) cout<<"#   ##";
+        if(i==6) cout<<"#    #";
+    }
+    if(s=='O'){
+        if(i==1) cout<<" #### ";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"#    #";
+        if(i==4) cout<<"#    #";
+        if(i==5) cout<<"#    #";
+        if(i==6) cout<<" #### ";
+    }
+    if(s=='P'){
+        if(i==1) cout<<"##### ";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"##### ";
+        if(i==4) cout<<"#     ";
+        if(i==5) cout<<"#     ";
+        if(i==6) cout<<"#     ";
+    }
+    if(s=='Q'){
+        if(i==1) cout<<" #### ";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"#    #";
+        if(i==4) cout<<"#  # #";
+        if(i==5) cout<<"#   ##";
+        if(i==6) cout<<" #### ";
+    }
+    if(s=='R'){
+        if(i==1) cout<<"##### ";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"#    #";
+        if(i==4) cout<<"##### ";
+        if(i==5) cout<<"#   # ";
+        if(i==6) cout<<"#    #";
+    }
+    if(s=='S'){
+        if(i==1) cout<<" #####";
+        if(i==2) cout<<"#     ";
+        if(i==3) cout<<" #### ";
+        if(i==4) cout<<"     #";
+        if(i==5) cout<<"     #";
+        if(i==6) cout<<"#####";
+    }
+    if(s=='T'){
+        if(i==1) cout<<"######";
+        if(i==2) cout<<"  #   ";
+        if(i==3) cout<<"  #   ";
+        if(i==4) cout<<"  #   ";
+        if(i==5) cout<<"  #   ";
+        if(i==6) cout<<"  #   ";
+    }
+    if(s=='U'){
+        if(i==1) cout<<"#    #";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"#    #";
+        if(i==4) cout<<"#    #";
+        if(i==5) cout<<"#    #";
+        if(i==6) cout<<" #### ";
+    }
+    if(s=='V'){
+        if(i==1) cout<<"#    #";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"#    #";
+        if(i==4) cout<<"#    #";
+        if(i==5) cout<<" #  # ";
+        if(i==6) cout<<"  ##  ";
+    }
+    if(s=='W'){
+        if(i==1) cout<<"#    #";
+        if(i==2) cout<<"#    #";
+        if(i==3) cout<<"#    #";
+        if(i==4) cout<<"#    #";
+        if(i==5) cout<<"# ## #";
+        if(i==6) cout<<" #  # ";
+    }
+    if(s=='X'){
+        if(i==1) cout<<"#    #";
+        if(i==2) cout<<" #  # ";
+        if(i==3) cout<<"  ##  ";
+        if(i==4) cout<<"  ##  ";
+        if(i==5) cout<<" #  # ";
+        if(i==6) cout<<"#    #";
+    }
+    if(s=='Y'){
+        if(i==1) cout<<"#   # ";
+        if(i==2) cout<<"#   # ";
+        if(i==3) cout<<" # #  ";
+        if(i==4) cout<<"  #   ";
+        if(i==5) cout<<"  #   ";
+        if(i==6) cout<<"  #   ";
+    }
+    if(s=='Z'){
+        if(i==1) cout<<"######";
+        if(i==2) cout<<"    # ";
+        if(i==3) cout<<"   #  ";
+        if(i==4) cout<<"  #   ";
+        if(i==5) cout<<" #    ";
+        if(i==6) cout<<"######";
+    }
+}
+int  main(){
+    string s;
+    cin>>s;
+    int n=s.length();
+    for(int i=0; i<n; i++){
+        s[i]=toupper(s[i]);
+    }
+    for(int i=1; i<=6; i++){
+        for(char x : s){
+            cout<<" ";
+            print(x, i);
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
 }
